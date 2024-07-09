36f09d5c39d96825762ac24f7c489f853d7de44e
Debora Monego <debora.monego@gmail.com>
2024-07-09 16:12:15 +0200

    Update main_sequence.py
    update main sequence for alignment changes

--- src/colbuilder/sequence/main_sequence.py
+++ src/colbuilder/sequence/main_sequence.py
@@ -1,34 +1,40 @@
-from colbuilder.sequence import muscle,system,modeller
+from colbuilder.sequence import align_sequences, modeller
 import subprocess
+import os
+from Bio import SeqIO
+from io import StringIO
 
-
-def build_sequence(path_wd=str,pdb_file=None,collagen_type=int,ensemble=int,
-                      dict_chain={},register=[],crosslink={}):
+def build_sequence(path_wd=str, fasta_file=None, collagen_type=int, ensemble=int,
+                   dict_chain={}, register=[], crosslink={}):
     """
-    
-    build sequence from an uncrossed collagen triple helix
-    
+    build fibril from an uncrossed collagen triple helix, starting from a FASTA file
     """
     print('-- Building the sequence of the Collagen Triple Helix --')
-
-    print('-- Read PDB-file --')
-    system_=system.System(pdb_filename=pdb_file,collagen_type=collagen_type,register=register)
-    system_.read_pdb(pdb_filename=pdb_file)
+    print('-- Read FASTA file --')
+    with open(fasta_file, 'r') as f:
+        fasta_content = f.read()
     
-    print('-- Convert PDB to Fasta format --')
-    system_.pdb_to_fasta(atoms=system_.atoms)
-
-    file_='tmp_'+"".join([i for i in register])
-    print('-- Sequence Alignment with Muscle: '+str(file_)+' --')
-    muscle_=muscle.Muscle(atoms=system_.atoms,file=file_)
-    muscle_.align_sequence(atoms=system_.atoms)
-
+    file_prefix = os.path.splitext(os.path.basename(fasta_file))[0]
+    file_ = f'tmp_{file_prefix}_{"".join(register)}'
+    
+    print(f'-- Sequence Alignment with Muscle: {file_} --')
+    aligned_sequences = align_sequences(fasta_content, file_)
+    
+    # Write aligned sequences to a file for MODELLER
+    aligned_file = f"{file_}.afa"
+    SeqIO.write(aligned_sequences, aligned_file, "fasta")
+    
     print('-- Prepare triple helical structure with MODELLER --')
-    modeller_=modeller.Modeller(system=system_,sequence=muscle_.sequence,file=file_,fasta=muscle_.fasta,ensemble=ensemble)
-    modeller_.prepare_alignment(muscle_file=file_+'.afa',register=register)
-    modeller_.run_modeller(alignment_file=file_,system=system_)
-
-    print('-- Save final PDB-file to '+str(pdb_file)+' --')
-    subprocess.run('cp '+str(modeller_.modeller_pdb)+' '+str(pdb_file)+'s',
-                       shell=True,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
-
+    modeller_ = modeller.Modeller(sequence=aligned_sequences, file=file_, fasta=aligned_file, ensemble=ensemble)
+    modeller_.prepare_alignment(muscle_file=aligned_file, register=register)
+    modeller_.run_modeller(alignment_file=file_)
+    
+    print(f'-- Save final PDB-file to {fasta_file.replace(".fasta", ".pdb")} --')
+    output_pdb = fasta_file.replace('.fasta', '.pdb')
+    subprocess.run(f'cp {modeller_.modeller_pdb} {output_pdb}',
+                   shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    
+    # Clean up temporary files
+    os.remove(aligned_file)
+    
+    return output_pdb
