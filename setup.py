f3f84a3cc586c1a70ad1efe60d686db032673171
Debora Monego <debora.monego@gmail.com>
2025-04-11 13:14:45 +0200

    fix dependencies

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='colbuilder',
-    version='0.1.0',
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    install_requires=[
-        "click",
-        "pydantic>=2.0",
-        "pyyaml",
-        "colorama"
-    ],
-    entry_points={
-        'console_scripts': [
-            'colbuilder=colbuilder.colbuilder:main',
-        ],
-    },
-)
+setup() 
